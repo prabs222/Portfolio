496cfcd1670cea89f8a22d880fc0dea7207ff50f
Prabhakar Mishra <prabhakarmishra222@gmail.com>
2023-11-30 20:43:46 +0530

    fixed errors

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -1,11 +1,4 @@
-import pennpixelimg from '../../assets/pennpixelimg.png'
-import passwordgeneratorimg from '../../assets/passwordgeneratorimg.png'
-import shoppingcartimg from '../../assets/shppingcartimg.png'
-import songifyimg from '../../assets/songifyimg.png'
-import studyhelpimg from '../../assets/studyhelpimg.png'
-import tictactoeimg from '../../assets/tictactoeimg.png'
-import topcoursesimg from '../../assets/topcoursesimg.png'
-import weatherappimg from '../../assets/weatherappimg.png'
+
 import eduChatimg from '../../assets/eduChatimg.png'
 
 
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import Logo from '../../assets/logo.gif'
 import './Sidebar.css'
 
 const Sidebar = () => {
